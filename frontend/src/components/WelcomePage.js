3d596d904d40cb20404a122bf28b3fbed72bea80
Amit <amit@softsuave.com>
2020-07-22 21:20:11 +0530

    testing image zoom and scroll

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -6,9 +6,14 @@ class WelcomePage extends Component {
         super(props);
         this.state = {
             width: 561,
-            height: 500
+            height: 500,
+            top:133,
+            left:391,
+            originalTop:133,
+            originalLeft:391,
         }
         this.myRef = React.createRef();
+        this.myRefZoom = React.createRef();
 
 
     }
@@ -33,10 +38,10 @@ class WelcomePage extends Component {
                            <img src={'https://live.staticflickr.com/4561/38054606355_26429c884f_b.jpg'} style={{width:'100%'}}/>
                         </div>
                     </div>*/}
-
+                    <div style={{position:'absolute', fontSize: '25px', zIndex:'1', top:this.state.top+'px', left:this.state.left+'px', color:'red'}}>.</div>
                     <div onWheel={this.onMouseWheel} style={{ width: '500px', height: '500px' }}>
-                        <PinchZoomPan >
-                            <img ref={this.myRef} alt='Test Image' src='http://picsum.photos/750/750' />
+                        <PinchZoomPan ref={this.myRefZoom} initialScale={1}>
+                            <img ref={this.myRef} alt='Test Image' src='https://live.staticflickr.com/4561/38054606355_26429c884f_b.jpg' />
                         </PinchZoomPan>
                     </div>
                 </div>
@@ -46,9 +51,18 @@ class WelcomePage extends Component {
 
     onMouseWheel = (event) => {
         setTimeout(()=>{
+/*
             let width = this.myRef.current.width
-            console.log(width)
-        },200)
+            console.log(this.myRef.current.style.transform)
+*/
+            let left=(this.myRefZoom.current.state.left +this.state.originalLeft)*((this.myRefZoom.current.state.scale))
+            let top=(this.myRefZoom.current.state.top +this.state.originalTop)*((this.myRefZoom.current.state.scale))
+            this.setState({left:left,top:top})
+            console.log(this.myRefZoom.current.state.left)
+            console.log(this.myRefZoom.current.state.top)
+
+
+        },500)
 
     }
 }
